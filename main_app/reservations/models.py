28be4b367625529dff7c9fe5ce569f6ec12fb930
Rodger <rodgef101@gmail.com>
2022-11-28 22:33:16 +0200

    view and models proof of concept need to finish view and add unit tests

--- main_app/reservations/models.py
+++ main_app/reservations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Rental(models.Model):
+	name = models.CharField(max_length=100)
+
+class Reservation(models.Model):
+    checkin = models.DateField()
+    checkout = models.DateField()
+
+    rental_id = models.ForeignKey(Rental, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["rental_id"]
--- main_app/reservations/views.py
+++ main_app/reservations/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Reservation
 
 # Create your views here.
+
+def index(request):
+    reservation_list = Reservation.objects.all().values
+    template = loader.get_template('reservations.html')
+    context = {
+        'reservations': reservation_list,
+    }
+    return HttpResponse(template.render(context, request))
