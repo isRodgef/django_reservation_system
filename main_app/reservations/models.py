9c7ee5c398b04fa6eefc4b2b299b7442ac194976
Rodger <rodgef101@gmail.com>
2022-11-29 22:19:26 +0200

    functions for previous resevations

--- main_app/reservations/models.py
+++ main_app/reservations/models.py
@@ -3,13 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Rental(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
 
 class Reservation(models.Model):
     checkin = models.DateField()
     checkout = models.DateField()
+    rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
 
-    rental_id = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    def get_prev_reservation_id(self):
+        last_reserved = Reservation.objects.filter(rental=self.rental, id__lt=self.id)
+        if last_reserved:
+            return last_reserved[len(last_reserved) - 1].id
+        return "--"
 
     class Meta:
         ordering = ["rental_id"]
+
+    
