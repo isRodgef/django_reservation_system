d4304ba6a61443680885ae16f7744644d1fc9525
Rodger <rodgef101@gmail.com>
2022-11-29 22:20:21 +0200

    view that renders template

--- main_app/reservations/views.py
+++ main_app/reservations/views.py
@@ -7,7 +7,8 @@ from .models import Reservation
 # Create your views here.
 
 def index(request):
-    reservation_list = Reservation.objects.all().values
+    reservation_list = Reservation.objects.all()
+    #import code; code.interact(local=dict(globals(), **locals()))
     template = loader.get_template('reservations.html')
     context = {
         'reservations': reservation_list,
